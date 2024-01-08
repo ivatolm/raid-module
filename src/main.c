5c52b7db6bbe949ac80caa21e433fd1e394957f0
Ivan Tolmachev <ivatolm@gmail.com>
2024-01-08 05:24:19 +0300

    Implement raid level 0

--- src/main.c
+++ src/main.c
@@ -9,15 +9,41 @@
 static struct rm_data {
 	struct gendisk *gd;
 
+	size_t stripe_size;
+
 	size_t disks_cnt;
 	struct block_device *disks[2];
 } rm;
 
+static void rm_submit_raid_level0(struct bio *bio)
+{
+	sector_t chunk_size = rm.stripe_size / rm.disks_cnt;
+
+	sector_t sector = bio->bi_iter.bi_sector;
+	sector_t size = bio->bi_iter.bi_size / KERNEL_SECTOR_SIZE;
+
+	while (size > 0) {
+		size_t disk_index = (sector / chunk_size) % rm.disks_cnt;
+		size_t clone_size = min(size, chunk_size);
+
+		struct bio *clone;
+		if (clone_size < bio_sectors(bio)) {
+			clone = bio_split(bio, clone_size, GFP_KERNEL, bio->bi_pool);
+			bio_chain(clone, bio);
+		} else {
+			clone = bio;
+		}
+
+		bio_set_dev(clone, rm.disks[disk_index]);
+		submit_bio(clone);
+
+		sector += clone_size;
+		size -= clone_size;
+	}
+}
+
 static void rm_submit_raid_level1(struct bio *bio)
 {
-	sector_t sector = bio->bi_iter.bi_sector;
-	sector_t size = bio->bi_iter.bi_size;
-
 	// Assuming there is at least 1 disk
 	if (bio_data_dir(bio) == READ)
 		goto submit_parent;
@@ -39,10 +65,10 @@ static void rm_submit_bio(struct bio *bio)
 	sector_t sector = bio->bi_iter.bi_sector;
 	sector_t size = bio->bi_iter.bi_size;
 	
-	pr_info("sector: %llu, size: %llu\n", sector, size);
+	pr_info("sector: %llu, size: %llu\n", sector, size / KERNEL_SECTOR_SIZE);
 
 	// Processing bio request with the RAID level 1 handler
-	rm_submit_raid_level1(bio);
+	rm_submit_raid_level0(bio);
 }
 
 struct block_device_operations rm_ops = {
@@ -68,6 +94,8 @@ static int rm_create_device(void)
 	rm.disks[0] = blkdev_get_by_path("/dev/sdb", FMODE_READ | FMODE_WRITE, NULL, NULL);
 	rm.disks[1] = blkdev_get_by_path("/dev/sdc", FMODE_READ | FMODE_WRITE, NULL, NULL);
 	rm.disks_cnt = 2;
+
+	rm.stripe_size = 16 * 1024 / 512;
 
 	int status = add_disk(rm.gd);
 
